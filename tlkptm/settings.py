11adb5a1dc16935f5ee2a34ac298bdfdb6656162
pkrashid123 <pkrashid123@gmail.com>
2025-03-29 15:04:50 +0530

    test

--- tlkptm/settings.py
+++ tlkptm/settings.py
@@ -121,10 +121,11 @@ USE_TZ = True
 STATIC_URL = '/static/'
 # STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 # STATIC_ROOT = os.path.join(BASE_DIR, 'static_build', 'static')
-STATIC_ROOT = BASE_DIR / 'static'
-
-MEDIA_URL = "/media/"
-MEDIA_ROOT = BASE_DIR/'media'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+# MEDIA_URL = "/media/"
+# MEDIA_ROOT = BASE_DIR/'media'
 
 
 # Default primary key field type
