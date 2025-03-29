d13f0bdb44709f5ffe903a5670847b533f5c4080
pkrashid123 <pkrashid123@gmail.com>
2025-03-29 16:50:09 +0530

    start

--- tlkptm/settings.py
+++ tlkptm/settings.py
@@ -120,13 +120,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = '/static/'
-# STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
-# MEDIA_URL = "/media/"
-# MEDIA_ROOT = BASE_DIR/'media'
+
+
+MEDIA_URL = "img/"
+MEDIA_ROOT = BASE_DIR/'media'
 
 
 # Default primary key field type
--- tlkptm/wsgi.py
+++ tlkptm/wsgi.py
@@ -1,4 +1,4 @@
-"""
+y"""
 WSGI config for tlkptm project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
@@ -8,8 +8,8 @@ https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
 """
 
 import os
-
 from django.core.wsgi import get_wsgi_application
+
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'tlkptm.settings')
 
